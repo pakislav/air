6f1f18258aa120bb8d8490f8e76934e41ef8baac
pakislav <7paki7@gmail.com>
2014-02-02 17:45:47 +0100

    svemirci
    ip wtf ???

--- Crypto_chat/src/hr/foi/air/kriptocavrljanje/ChatActivity.java
+++ Crypto_chat/src/hr/foi/air/kriptocavrljanje/ChatActivity.java
@@ -154,7 +154,7 @@ public class ChatActivity extends Activity {
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		
+		// server
 		
 		class ChatHelperThread extends Thread{
 			
@@ -346,14 +346,7 @@ public class ChatActivity extends Activity {
 		}
 		
 		new ChatHelperThread().start();
-		
-		
-		
-		
-		
-		
-		
-		
+		// zavr�etak servera
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		
 		chatList = (ListView) findViewById(R.id.listView_chat);
@@ -375,11 +368,169 @@ public class ChatActivity extends Activity {
 				if (!sendMessage.getText().toString().isEmpty()) {
 					adapter.add(new Comment(sendMessage.getText().toString(),
 							false));
-					sendMessage.setText("");
+					//////////////////////////////////////////////////////////////////////////////
+					
+						
+						/**
+						 * Paket verzija 14.01.31.a
+						 * @author student
+						 *
+						 */
+						class Paket{ // predstavlja strukturu svakog paketa naseg protokola
+							// fiksnih 20 + 20 + 4 okteta
+							private String mSalje = null; // SHA1(javni kljuc) hex onoga tko salje poruku ili 20 nula
+							private String mPrima = null; // SHA1(javni kljuc) hex onoga kome je poruka namjenjena ili 20 nula
+							private Short mDuljinaPoruke = null; // do 32767 okteta moze biti dug sadrzaj poruke (4 hex) ili 4 nule
+							// varijabilni dio poruke 4 + n okteta (gdje je n < 32763)
+							private Short mBezPaddinga = null; // duljina poruke nakon dekripcije (4 hex) ili 4 nule (fiksno)
+							private String mPoruka = null; // sama poruka
+							
+							// dakle ukupno paket uvijek ima do (20 + 20 + 4) + [32767 zaokruzeno na blok asimetricnog outputa]
+							public final static int PAKET_LEN_MAX = 40000; //XXX cca
+							public final static String NULA20 = "00000000000000000000";
+							
+							public Paket(){
+								mSalje = NULA20;
+								mPrima = NULA20;
+								mDuljinaPoruke = 0;
+								mBezPaddinga = 0;
+								mPoruka = null;
+							}
+							/**
+							 * 
+							 * @param Salje SHA1 u hex (20)
+							 * @param Prima SHA1 u hex (20)
+							 * @param Poruka
+							 */
+							public Paket(String Salje, String Prima, String Poruka){
+								//TODO validacija Salje, Prima, Poruka (duzine, sadrzaj, itd.)
+								mSalje = Salje;
+								mPrima = Prima;
+								//XXX kriptiranje...
+								mDuljinaPoruke = (short)(Poruka.length() + 4);
+								mBezPaddinga = (short)(Poruka.length());
+								mPoruka = Poruka;
+							}
+							
+							public void deserijaliziraj(byte[] niz){
+								/* XXX
+								 * Za one koji zele znati vise:
+								 * "mSalje = new String(niz, 0, 20);"
+								 * , zasto ovaj nacin rusi JVM?
+								 * # JRE version: 6.0_38-b05
+								 * # Java VM: Java HotSpot(TM) 64-Bit Server VM (20.13-b02 mixed mode linux-amd64 compressed oops)
+								 * # Problematic frame:
+								 * # V  [libjvm.so+0x4e31c7]  java_lang_String::utf8_length(oopDesc*)+0x67
+								 */
+								StringBuffer tmp = new StringBuffer();
+								for(int i = 0; i < 20; ++i){
+									tmp.append((char)niz[i]);
+								}
+								mSalje = tmp.toString(); //System.out.println(String.format("%s", mSalje));
+								tmp.delete(0, tmp.length());
+								for(int i = 20; i < 40; ++i){
+									tmp.append((char)niz[i]);
+								}
+								mPrima = tmp.toString(); //System.out.println(String.format("%s", mPrima));
+								tmp.delete(0, tmp.length());
+								for(int i = 40; i < 44; ++i){
+									tmp.append((char)niz[i]);
+								}
+								mDuljinaPoruke = Short.parseShort(tmp.toString(), 16); //System.out.println(String.format("%d", mDuljinaPoruke));
+								tmp.delete(0, tmp.length());
+								//TODO validacija duljine nastavka prema mDuljinaPoruke
+								//XXX krece kriptirani dio
+								for(int i = 44; i < 48; ++i){
+									tmp.append((char)niz[i]);
+								}
+								mBezPaddinga = Short.parseShort(tmp.toString(), 16); //System.out.println(String.format("%d", mBezPaddinga));
+								tmp.delete(0, tmp.length());
+								for(int i = 48; i < (int)mBezPaddinga + 48; ++i){
+									tmp.append((char)niz[i]);
+								}
+								mPoruka = tmp.toString(); //System.out.println(String.format("%s", mPoruka));			
+							}
+							
+							public boolean jeZahtjevZaRegistracijom(){ // registriraj me
+								if(mPrima.compareTo(NULA20) == 0){
+									return true;
+								}
+								return false;
+							}
+							
+							public boolean jeZahtjevZaPopisom(){ // tko je online?
+								if(mPrima.compareTo(mSalje) == 0){
+									return true;
+								}
+								return false;
+							}
+							
+							public String getSalje(){
+								return mSalje;
+							}
+							
+							public String getPoruka(){
+								return mPoruka;
+							}
+							
+
+							public byte[] serijaliziraj(){
+								byte[] niz = new byte[PAKET_LEN_MAX];
+								for(int i = 0; i < 20; ++i){
+									niz[i] = (byte)mSalje.charAt(i);
+								}
+								for(int i = 0; i < 20; ++i){
+									niz[i + 20] = (byte)mPrima.charAt(i);
+								}
+								String tmp = String.format("%04x", mDuljinaPoruke);
+								for(int i = 0; i < 4; ++i){
+									niz[i + 40] = (byte)tmp.charAt(i);
+								}
+								//XXX kriptirano...
+								tmp = String.format("%04x", mBezPaddinga);
+								for(int i = 0; i < 4; ++i){
+									niz[i + 44] = (byte)tmp.charAt(i);
+								}
+								for(int i = 0; i < mBezPaddinga; ++i){ //XXX mBezPaddinga mora biti mPoruka.length !
+									niz[i + 48] = (byte)mPoruka.charAt(i);
+								}
+								return niz;
+							}
+						}
+					
+						final int IN_BUF_LEN = Paket.PAKET_LEN_MAX;
+
+						
+						try {
+							DatagramSocket s = new DatagramSocket(9002, Inet4Address.getByName(ip));
+							Paket p = new Paket(hash, sugovornik, "lidija bacic");
+							byte[] pporuka = p.serijaliziraj();
+							Log.d("-aaa-------->", ip.toLowerCase());
+							DatagramPacket poruka = new DatagramPacket(pporuka, pporuka.length, sugovornikIp, sugorovnikPort);
+							
+							Log.d("--salji-------->", ip);
+							s.send(poruka);
+							Log.d("------------------->", "saljem");
+							/*
+							DatagramPacket odgovor = new DatagramPacket(in, in.length);
+							s.receive(odgovor);
+							Paket podgovor = new Paket();
+							podgovor.deserijaliziraj(in);
+							System.out.println(podgovor.mPoruka);
+							*/
+
+						} catch (Exception e) {
+							e.printStackTrace();
+							Log.d("-------11111111111------------>", "saljem");
+						}
+						///////////////////////////////////////////////////
+				
+				
+				sendMessage.setText("");
 				}
 			}
-		});
-
+			});
+		
 		sendMessage = (EditText) findViewById(R.id.editTxt_message);
 		sendMessage.setOnKeyListener(new OnKeyListener() {
 
